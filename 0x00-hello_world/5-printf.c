04d71e90d7c793ecb7066655ca2026c1e342e4ef
root <adetunji4325@gmail.com>
2022-03-10 19:08:19 -0800

    hello

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,8 @@
  * main - Entry point
  *
  * Description: prints 
- * "with proper grammar, but the outcome is a piece of art,"
+ * "with proper grammar, but the outcome is a piece of 
+ * * art,"
  *
  * Return: Always 0 (Success)
  */
